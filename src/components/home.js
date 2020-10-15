de8ad9122afce64be54040291b97c1e6582058bc
Juansito_the_dev <tecnosebastube@gmail.com>
2020-10-15 11:22:10 -0500

    a publicar

--- src/components/home.js
+++ src/components/home.js
@@ -4,15 +4,28 @@ import './styles/general.css';
 import Menu from './mini-c/Menu.js';
 
 class Home extends React.Component {
+    state = {}
     handleClick = e => {
-        alert("you clicked me!!!!")
+        alert(this.state.firstName + " " + this.state.lastName)
+    }
+    handleChange = e => {
+        // console.log(e.target.value);
+        this.setState({
+            [e.target.name]: e.target.value,
+        })
+    }
+    handleSubmit = e => {
+        e.preventDefault();
     }
     render() {
-        const prop = this.props;
         return (
             <div>
-                <Menu names={prop.names} last={prop.last} img={logo}/>
-                <button onClick={this.handleClick}>click me!</button>
+                <Menu names={this.state.firstName} last={this.state.lastName} img={logo}/>
+                <form action='#' onSubmit={this.handleSubmit}>
+                    <input type="text" name="firstName" placeholder="firstName" onChange={this.handleChange} />
+                    <input type="text" name="lastName" placeholder="lastName" onChange={this.handleChange} />
+                    <button onClick={this.handleClick}>click me!</button>
+                </form>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import Home from './components/home';
 
-const name = "Juan Sebastian";
-const lastname = "Florez Gomez";
 const container = document.getElementById('root');
 
-ReactDOM.render(<Home names={name} last={lastname}/>, container);
+ReactDOM.render(<Home />, container);
